0c5f511897cb7264b3ccda2caf18cac51ad91d78
snowvvv <bucholzevb@mail.ru>
2021-11-23 19:43:21 +0500

    documentation

--- filter.py
+++ filter.py
@@ -1,8 +1,15 @@
 from PIL import Image
 import numpy as np
 
-
 def get_color(matrix, x_start, y_start, gradation):
+    """
+
+    :param matrix: матрица
+    :param x_start: начальные положения x
+    :param y_start: и y
+    :param gradation: количество градаций серого цвета,
+    :return: возвращает значение серого цвета, которое после исползуется для формирования изображения в методе make_mosaic
+    """
     grey = 0
     for x in range(x_start, x_start + gradation):
         for y in range(y_start, y_start + gradation):
@@ -15,6 +22,17 @@ def get_color(matrix, x_start, y_start, gradation):
 
 
 def get_matrix(matrix, x_start, y_start, gradation, grey, gray_gradation):
+    """
+
+    :param matrix: основная матрица
+    :param x_start: начальные положения x и y
+    :param y_start: -
+    :param gradation: степень градации серого цвета, используется для расчета переменной gray
+    :param grey: основной параметр серого цвета.
+    :param gray_gradation: Вводимое с клавиатуры значение, обозначает градацию серого цвета
+    (50 как коэффициент, можно убрать, но я не вижу причин)
+    :return: возвращает двумерный массив, по которому формируется результативное изображение
+    """
     grey_gradations = gray_gradation * 50
     for x in range(x_start, x_start + gradation):
         for y in range(y_start, y_start + gradation):
@@ -22,6 +40,13 @@ def get_matrix(matrix, x_start, y_start, gradation, grey, gray_gradation):
 
 
 def make_mosaic(img, gradation, grey_gradation):
+    """
+
+    :param img: исходное изображение
+    :param gradation: шаг обработки
+    :param grey_gradation: водимое с клавиатуры значение, обозначает градацию серого цвета
+    :return: формирует картинку из массива matrix
+    """
     matrix = np.array(img)
     height = len(matrix)
     width = len(matrix[1])
@@ -33,6 +58,10 @@ def make_mosaic(img, gradation, grey_gradation):
 
 
 def main():
+    """
+
+    :return: ничего не возвращает, просто сохраняет изображение в формате jpg
+    """
     print('Введите название исходного изображения:')
     img = Image.open(input())
     print("Введите степень пикселизации изображения  серого цвета:")       #размер блока (чем больше шаг, тем сильнее пикселизация)
